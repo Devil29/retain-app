20557a8229232b5dd266ae493a95dbd87f05c8ba
Devil29 <vishalraj48@gmail.com>
2016-09-06 13:49:29 +0530

    Worked till module 5

--- src/app/containers/main.ts
+++ src/app/containers/main.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { AppBar } from '../ui';
+import { Notes } from './notes';
 
 @Component({
   selector: 'main-container',
   directives:[
-  	AppBar
+  	AppBar,
+  	Notes
   ],
   template: `
     <div>
       <main class="main">
       	<app-bar></app-bar>
-      	Content will go here
+      	<notes-container></notes-container>
       </main>
     </div>
   `
